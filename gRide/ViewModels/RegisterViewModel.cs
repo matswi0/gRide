e2a6878d43e97390933d14c012dc7577271ae953
matswi0 <matswi18ii@gmail.com>
2022-08-31 21:44:47 +0200

    register

---
+++ gRide/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gRide.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(dataType:DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
