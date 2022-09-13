2ff6137edd48a4f738977b32f57961537b104bab
matswi0 <matswi18ii@gmail.com>
2022-09-13 19:39:56 +0200

    openlayers/ ol-geocoder

---
+++ gRide/Controllers/EventsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace gRide.Controllers
+{
+    public class EventsController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
